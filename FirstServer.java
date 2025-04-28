2221b0cb5a57eac1045e8d00c02e165be16e6171
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-04-27 21:23:04 -0400

    Practicing creating Sockets

---
+++ FirstServer.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class FirstServer {
+  public static void main(String[] args) {
+
+    try {
+      ServerSocket firstServerSocket = new ServerSocket(6868);
+      Socket socketConnection = firstServerSocket.accept();
+
+      DataInputStream dataStreamIn = new DataInputStream(socketConnection.getInputStream());
+      String readString = (String) dataStreamIn.readUTF();
+      System.out.println("message = " + readString);
+
+      firstServerSocket.close();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
